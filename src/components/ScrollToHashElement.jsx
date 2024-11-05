bb321367d718a96411d58a926c7543de59049042
Camilla De Pretto <161570720+camilla-cs@users.noreply.github.com>
2024-11-05 16:52:32 +1100

    feat: linked navbar to sections

---
+++ src/components/ScrollToHashElement.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+// import ScrollToHashElement from "@cascadia-code/scroll-to-hash-element"; 
+
+// function ScrollToElement() {
+
+//     return (
+//         <ScrollToHashElement behavior="smooth" inline="center" block="center"/>
+//     )
+// }
+
+// export default ScrollToElement 
+
